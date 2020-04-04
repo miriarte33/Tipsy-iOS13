c2c460658b7db9fe5ed859a13dcee8c22c52d7ca
Manrique Iriarte <m_iriarte33@yahoo.com>
2020-04-04 16:53:01 -0400

    ViewController and segue to ResultsViewController

---
+++ Tipsy/Controller/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Manrique Iriarte on 4/4/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    var tip: Float = 10.0;
+    var people: Int = 2;
+    var split: String = "0.00";
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- Tipsy/Controller/ViewController.swift
+++ Tipsy/Controller/ViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var zeroPercentButton: UIButton!
+    @IBOutlet weak var tenPercentButton: UIButton!
+    @IBOutlet weak var twentyPercentButton: UIButton!
+    @IBOutlet weak var totalBillTextField: UITextField!
+    @IBOutlet weak var peopleAmountLabel: UILabel!
+    
+    var splitCalculator = SplitCalculator()
+    
+    var buttons: Array<UIButton>? = nil
+    
+    var tip: Float = 10.0
+    var people: Int = 2
+    var billTotal: Float? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttons = [zeroPercentButton, tenPercentButton, twentyPercentButton]
     }
 
-
+    @IBAction func tipButtonPressed(_ sender: UIButton) {
+        for button in buttons! {
+            if button.currentTitle! == sender.currentTitle! {
+                button.isSelected = true;
+                
+                tip = Float(button.currentTitle!.dropLast())! / 100.0;
+            } else {
+                button.isSelected = false;
+            }
+        }
+    }
+    
+    @IBAction func stepperPressed(_ sender: UIStepper) {
+        people = Int(sender.value)
+        peopleAmountLabel.text = String(people)
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        billTotal = Float(totalBillTextField.text!) ?? 0.0
+        
+        splitCalculator.calculateSplit(billTotal ?? 0.0, tip, people)
+        
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "goToResults" {
+            let destination = segue.destination as! ResultsViewController
+            destination.people = people
+            destination.split = splitCalculator.getSplit()
+            destination.tip = tip
+        }
+    }
 }
 
